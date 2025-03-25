0c1ae7211bdc4ea6f8511053c86f1ba54864b58c
thanhtung795 <vothanhtung795@gmail.com>
2025-03-25 14:33:25 +0700

    fix cau truc file

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import { Banner } from "@/app/components/banner";
-import { ReviewForm } from "@/app/components/review-form";
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      {/* Banner component */}
-      <Banner />
-
-      <div className="container mx-auto px-4 py-8">
-        {/* Tiêu đề trang */}
-        <h1 className="text-3xl font-bold text-center mb-8">
-          Đánh Giá Chất Lượng Nhà Hàng
-        </h1>
-
-        {/* Form đánh giá */}
-        <div className="max-w-4xl mx-auto">
-          <ReviewForm />
-        </div>
-      </div>
-    </main>
-  );
+function Page() {
+  return <div></div>;
 }
+
+export default Page;
---
+++ app/pages/RestaurantReview/RestaurantReview.tsx
@@ -0,0 +1,38 @@
+import { Banner } from "@/app/components/banner";
+import { ReviewForm } from "@/app/components/review-form";
+import WebConfig from "@/app/config/WebConfig";
+import AnimatedContent from "@/app/components/ui/Animation/AnimatedContent";
+
+export default function RestaurantReview() {
+  return (
+    <main className="min-h-screen bg-background">
+      <Banner />
+      <div className="relative w-full h-fit flex flex-col justify-center items-center">
+        <div
+          className="absolute inset-0 bg-cover bg-center filter blur-8 opacity-80"
+          style={{ backgroundImage: `url(${WebConfig.getBackground})` }}
+        ></div>
+
+        <div className="relative container h[960px] mx-auto px-4 -mt-3 py-8">
+          <h1 className="text-3xl font-semibold text-black bg-gray-100 filter opacity-80 w-fit justify-center mx-auto rounded-full p-2 text-center mb-2">
+            Khảo sát chất lượng và dịch vụ nhà hàng
+          </h1>
+          <div className="max-w-4xl mx-auto">
+            <AnimatedContent
+              distance={150}
+              direction="horizontal"
+              reverse={false}
+              config={{ tension: 80, friction: 20 }}
+              initialOpacity={0.2}
+              animateOpacity
+              scale={1.1}
+              threshold={0.2}
+            >
+              <ReviewForm />
+            </AnimatedContent>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
